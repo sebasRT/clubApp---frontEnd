17e2356fcf34bafa4717025fa579f2b011705ff8
sebasRT <rtsebas11@gmail.com>
2024-03-19 11:58:05 -0400

    fix: image importing typo

--- src/app/(user)/login/page.tsx
+++ src/app/(user)/login/page.tsx
@@ -1,4 +1,4 @@
-import loginTeamImage from "@public/LoginTeamImage.png"
+import loginTeamImage from "@public/loginTeamImage.png"
 import Image from "next/image"
 import Link from "next/link"
 import LoginUserForm from "./LoginUserForm"
