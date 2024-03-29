c7d4930ffa4fc734e6a1da17cdcac442fef7fcfb
Pablo Alvarez <pipualvarez1@gmail.com>
2024-03-29 17:37:40 -0300

    ui player page

---
+++ src/app/(user)/components/nextPractice/NextPractice.tsx
@@ -0,0 +1,14 @@
+const NextPractice = () => {
+  return (
+    <div className="m-2">
+      <section className="text-[#363545] bg-[#D6D6E9] p-3">
+        <p className="font-[700]">Fecha y hora:</p>
+        <p className="font-[700]">Lugar:</p>
+        <p className="font-[700]">Duración estimada:</p>
+        <p className="font-[700]">Instrucciones:</p>
+        <p className="font-[700]">Entrenador:</p>
+      </section>
+    </div>
+  )
+}
+export default NextPractice
--- src/app/(user)/user/page.tsx
+++ src/app/(user)/user/page.tsx
@@ -1,33 +1,37 @@
-import NextMatchContainer from "@/components/infoBlock/nextMatch/NextMatchContainer";
+
 import { getData } from "@/lib/user.actions";
-import footballUserBanner from "@/public/footballUserBanner.png";
+import footballUserBanner from "@/public/footballUserBanner.webp";
 import bgImage from '@/public/primaryBG.png';
 import Image from "next/image";
 import Stadistic from "../components/stadisticsPlayer/Stadistic";
+import NextUserMatch from "../components/nextUserMatches/NextUserMatch";
+import NextMatchUserContainer from "../components/nextUserMatches/NextMatchUserContainer";
+import NextPracticeContainer from "../components/nextPractice/NextPracticeContainer";
 
 export default async function page() {
   const data = await getData()
   
   const cards = [
-    <NextMatchContainer key="1" />,
-    <Stadistic key="2" />,
+    <NextMatchUserContainer key="1" />,
+    <NextPracticeContainer key="2"/>,
+    <Stadistic key="3" />,
   ];
 
   return (
     <main className="min-h-screen w-screen flex flex-col items-center relative">
-      <Image src={bgImage} alt="login Team Image" className="absolute object-cover w-full h-full -z-10" />
+      <Image src={bgImage} alt="login Team Image" className="absolute object-cover  h-full -z-10" />
       <figure className="w-full hidden md:block">
-        <Image src={footballUserBanner} className="object-cover" alt={"Football banner"} />
+        <Image src={footballUserBanner} className="object-cover w-full" alt={"Football banner"} />
       </figure>
-      <div className="mt-24 md:my-1">
-        <h1 className="text-center text-5xl font-bauhs text-baltic-sea-900">Bienvenid@ <span className="text-6xl">{`${data.userName} ${data.userLastname}`}</span></h1>
+      <div className="mt-24 md:my-3">
+        <h1 className="text-center text-5xl font-bauhs text-baltic-sea-900">Bienvenid@ <span>{`${data.userName} ${data.userLastname}`}</span></h1>
       </div>
       <div className="flex justify-between shadow-2xl md:px-5">
       <div className="overflow-hidden">
-        <div className="flex flex-col transition-transform duration-500 ease-in-out transform lg:flex-row">
+        <div className="flex flex-col transition-transform duration-500 ease-in-out transform lg:flex-row ">
           {cards.map((card, index) => {
               return (
-                <div key={index} className="w-[20rem] min-[425px]:w-[26rem] md:w-[40rem] lg:w-[30rem] min-[1440px]:w-[40rem] p-4">
+                <div key={index} className="w-[20rem] min-[425px]:w-[26rem] md:w-[40rem] lg:w-[20rem] min-[1440px]:w-[30rem] p-1">
                   {card}
                 </div>
               );
