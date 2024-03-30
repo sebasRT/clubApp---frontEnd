a5e25dbea5dc1eff7d603c009edf225a5870f09e
Pablo Alvarez <pipualvarez1@gmail.com>
2024-03-30 14:10:54 -0300

    aboutUs Information

--- src/components/infoBlock/aboutUs/AboutUs.tsx
+++ src/components/infoBlock/aboutUs/AboutUs.tsx
@@ -1,8 +1,14 @@
+import AboutUsInfo from "./data"
+
 const AboutUs = () => {
   return (
     <div>
-      <section className={`bg-silver-100 lg:px-16 py-1 rounded-[5px] m-2 text-center`}>
-        <p className={`text-gray-600`}>Sobre Nosotros</p>
+      <section className={`bg-silver-100 lg:px-16 py-1 rounded-[5px] m-2`}>
+      {AboutUsInfo.map((info: any) => (
+        <div
+          key={info.id}
+          dangerouslySetInnerHTML={{ __html: info.info }}></div>
+        ))}
       </section>
     </div>
   )
---
+++ src/components/infoBlock/aboutUs/data.ts
@@ -0,0 +1,30 @@
+const AboutUsInfo = [
+  {
+  id: 1,
+  info: `En <strong>ClubApp</strong>, nos apasiona el fútbol tanto como a ti. <br/><br/>
+    Sabemos que dirigir un club de fútbol implica una gran responsabilidad y dedicación. Es por eso que creamos <strong>ClubApp</strong>: para simplificar y mejorar cada aspecto de la gestión de tu club, desde la administración de jugadores hasta la interacción con los aficionados.<br/><br/>
+    Con <strong>ClubApp</strong>, los clubes pueden optimizar sus operaciones internas y ofrecer a los aficionados una experiencia más completa y accesible.<br/><br/>
+
+  <strong>¿Qué Hace ClubApp?</strong><br/>
+  
+  <strong>Gestión de Jugadores y Directores Técnicos</strong>: Mantén un registro detallado de tus jugadores y directores técnicos, incluyendo estadísticas, historial de partidos y evaluaciones de desempeño.<br/>
+  
+  <strong>Calendario y Fixtures</strong>: Planifica y comunica fácilmente los horarios de partidos, entrenamientos y eventos del club.<br/>
+  
+  <strong>Información del Club</strong>: Comparte la historia, logros y noticias relevantes de tu club con tus seguidores.<br/>
+  
+  <strong>Interacción con Aficionados</strong>: Conecta con tus seguidores a través de la info de los partidos por jugar y jugados<br/><br/>
+  
+  <strong>Nuestra Misión</strong><br/>
+  
+  En <strong>ClubApp</strong>, nuestra misión es apoyar a los clubes de fútbol en su camino hacia el éxito. Nos esforzamos por proporcionar las herramientas más avanzadas y fáciles de usar para que puedas concentrarte en lo que más importa: <strong>el juego y la comunidad que lo rodea</strong>.<br/><br/>
+  
+  Únete a la Familia de ClubApp<br/><br/>
+  
+  Únete a miles de clubes de fútbol en todo el mundo que confían en <strong>ClubApp</strong> para simplificar su gestión y fortalecer su conexión con los aficionados. Descarga <strong>ClubApp</Strong> hoy mismo y descubre cómo podemos ayudar a llevar tu club al siguiente nivel.<br/><br/>
+  
+  <strong>¡Gracias por elegir ClubApp!</strong>`
+  }
+]
+
+export default AboutUsInfo
