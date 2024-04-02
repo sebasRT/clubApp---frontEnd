2d1e04c5929020a6f43562dca1f3c56ee91ede48
Pablo Alvarez <pipualvarez1@gmail.com>
2024-04-02 17:20:54 -0300

    entrenamiento info

--- src/app/(user)/components/nextPractice/NextPractice.tsx
+++ src/app/(user)/components/nextPractice/NextPractice.tsx
@@ -2,11 +2,11 @@ const NextPractice = () => {
   return (
     <div className="m-2">
       <section className="text-[#363545] bg-[#D6D6E9] p-3">
-        <p className="font-[700]">Fecha y hora:</p>
-        <p className="font-[700]">Lugar:</p>
-        <p className="font-[700]">Duración estimada:</p>
+        <p className="font-[700]">Fecha y hora: 2024-03-18  14:00pm</p>
+        <p className="font-[700]">Lugar: Parque municipal</p>
+        <p className="font-[700]">Duración estimada: 14:00pm - 16:00pm</p>
         <p className="font-[700]">Instrucciones:</p>
-        <p className="font-[700]">Entrenador:</p>
+        <p className="font-[700]">Entrenador: Gonzales</p>
       </section>
     </div>
   )
