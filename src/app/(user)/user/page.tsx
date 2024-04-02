8c1ffa886ec042dfcd47e368b491d60206029a08
sebasRT <rtsebas11@gmail.com>
2024-04-02 17:21:01 -0400

    user page up to date

--- src/app/(user)/user/page.tsx
+++ src/app/(user)/user/page.tsx
@@ -1,6 +1,3 @@
-
-
-
 import { getMatchByCategory, getPlayerInfo } from "@/lib/user.actions";
 import footballUserBanner from "@/public/footballUserBanner.webp";
 import bgImage from '@/public/primaryBG.png';
@@ -13,10 +10,10 @@ import ModalNextMatch from "../components/ModalNextMatch";
 export default async function page() {
   const data = await getPlayerInfo()
   const match = await getMatchByCategory();
-  
+
   const cards = [
     <NextMatchUserContainer key="1" />,
-    <NextPracticeContainer key="2"/>,
+    <NextPracticeContainer key="2" />,
     <Stadistic key="3" />,
   ];
 
@@ -27,24 +24,23 @@ export default async function page() {
         <Image src={footballUserBanner} className="object-cover w-full" alt={"Football banner"} />
       </figure>
       <div className="mt-24 md:my-3">
-      <ModalNextMatch data={data} match={match}/>
+        <ModalNextMatch data={data} match={match} />
         <h1 className="text-center text-5xl font-bauhs text-baltic-sea-900">Bienvenid@ <span>{`${data.userName} ${data.userLastname}`}</span></h1>
       </div>
       <div className="flex justify-between shadow-2xl md:px-5">
-      <div className="overflow-hidden">
-        <div className="flex flex-col transition-transform duration-500 ease-in-out transform lg:flex-row ">
-          {cards.map((card, index) => {
+        <div className="overflow-hidden">
+          <div className="flex flex-col transition-transform duration-500 ease-in-out transform lg:flex-row ">
+            {cards.map((card, index) => {
               return (
                 <div key={index} className="w-[20rem] min-[425px]:w-[26rem] md:w-[40rem] lg:w-[20rem] min-[1440px]:w-[30rem] p-1">
                   {card}
                 </div>
               );
             }
-          )}
+            )}
+          </div>
         </div>
       </div>
-    </div>
     </main>
   );
 };
-
