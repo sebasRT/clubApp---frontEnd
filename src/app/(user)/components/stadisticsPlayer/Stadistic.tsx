f14cf01b20cb5b2a93bbcba09a6d0ac40bb2ba3e
sebasRT <rtsebas11@gmail.com>
2024-04-03 12:01:08 -0400

    fix: closeModal, setCloseModal

--- src/app/(user)/components/stadisticsPlayer/Stadistic.tsx
+++ src/app/(user)/components/stadisticsPlayer/Stadistic.tsx
@@ -1,33 +1,37 @@
-const Stadistic = () => {
+const Statistics = () => {
   return (
-    <div className={`bg-baltic-sea-900 px-2 py-5 rounded-[5px]`}>
+    <div className="bg-baltic-sea-900 px-2 py-5 rounded-5">
       <section>
-        <h2 className={`text-center text-xl font-squada text-primary-500`}>
-          ESTADISTICAS
+        <h2 className="text-center text-xl font-squada text-primary-500">
+          ESTADÍSTICAS
         </h2>
       </section>
-      <div className={`bg-[#BCBBCE] text-[#363545] rounded-[5px] p-0 h-40 font-squada  overflow-y-scroll `}>
-        <table className="border-collapse  w-full table-auto text-center">
-          <tr>
-            <th className="border border-black">#</th>
-            <th className="border border-black">nombre</th>
-            <th className="border border-black">posicion</th>
-            <th className="border border-black">goles</th>
-            <th className="border border-black">asis.</th>
-            <th className="border border-black">tarj.</th>
-          </tr>
-
-          <tr>
-            <td className="border border-black">10</td>
-            <td className="border border-black">Pablo Alvarez</td>
-            <td className="border border-black"> delantero</td>
-            <td className="border border-black"> 5</td>
-            <td className="border border-black"> 2</td>
-            <td className="border border-black"> 1</td>
-          </tr>
+      <div className="bg-gray-300 text-[#363545] rounded-5 p-0 h-40 font-squada overflow-y-scroll">
+        <table className="border-collapse w-full table-auto text-center">
+          <thead>
+            <tr>
+              <th className="border border-black">#</th>
+              <th className="border border-black">Nombre</th>
+              <th className="border border-black">Posición</th>
+              <th className="border border-black">Goles</th>
+              <th className="border border-black">Asist.</th>
+              <th className="border border-black">Tarj.</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="border border-black">10</td>
+              <td className="border border-black">Pablo Alvarez</td>
+              <td className="border border-black">Delantero</td>
+              <td className="border border-black">5</td>
+              <td className="border border-black">2</td>
+              <td className="border border-black">1</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
   );
 };
-export default Stadistic;
+
+export default Statistics;
